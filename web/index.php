86863ce4ff57e71240bd45de3b727cc1d6e9074e
{TherinavdW} <therina07@yahoo.com>
2016-08-18 10:16:22 +0200

    Add heroku files

---
+++ web/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Your name goes here</title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>Hello World</h1>";
+        ?>
+    </body>
+</html>
